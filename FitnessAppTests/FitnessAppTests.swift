e6acce79cdea012597ae6153e57e3585c7181af7
Saputra Budianto <craftgirlsssshopping@gmail.com>
2025-01-03 09:50:34 +0700

    Initial Commit

---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by XFCE on 03/01/25.
+//
+
+import Testing
+@testable import FitnessApp
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
